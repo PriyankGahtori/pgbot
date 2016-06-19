0b05c933e8a65409c6ac15fed56bd8c08ebaeec6
Priyank Gahtori <priyank.gahtori@gmail.com>
2016-06-19 14:57:42 +0530

    added validation

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ app.get('/hello', function (req, res) {
   res.send('Hello World!');
 });
 
-//fecebook webhook testing
+//facebook webhook testing
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+      req.query['hub.verify_token'] === 'pgboot_2016') {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
